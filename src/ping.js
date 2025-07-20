9c80cf44dedeac085f4cab921144db99e3994ca8
LeaOhyeah <leasehat@gmail.com>
2025-07-20 10:09:31 +0800

    option: summary stats, realtime output json & csv

--- src/ping.js
+++ src/ping.js
@@ -1,13 +1,14 @@
-import http from 'k6/http';
-import { sleep, check } from 'k6';
+import http from "k6/http";
+import { sleep, check } from "k6";
 
 export const options = {
   vus: 10, // virtual users
-  duration: '5s', // max duration
+  duration: "5s", // max duration
+  summaryTrendStats: ["avg", "min", "med", "max", "p(99)"], // statistik output, p = persentil
 };
 
-export default function() {
-  let res = http.get('http://localhost:3000/ping');
+export default function () {
+  let res = http.get("http://localhost:3000/ping");
   check(res, { "status is 200": (res) => res.status === 200 });
   sleep(1); // sleep before looping
 }
